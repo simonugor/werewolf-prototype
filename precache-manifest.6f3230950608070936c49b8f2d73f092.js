4958184c7020fa121f3107ed9896dec299ab5e67
simonugor <61743750+simonugor@users.noreply.github.com>
2020-10-20 02:27:42 +0200

    Updates

---
+++ precache-manifest.6f3230950608070936c49b8f2d73f092.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec88a7a8d955bfa69278db5a14ac74",
+    "url": "/werewolf-prototype/index.html"
+  },
+  {
+    "revision": "0e7d44e3d7be51c713b9",
+    "url": "/werewolf-prototype/static/css/main.55b0979d.chunk.css"
+  },
+  {
+    "revision": "ba23c281ed32f29504aa",
+    "url": "/werewolf-prototype/static/js/2.0155c972.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/werewolf-prototype/static/js/2.0155c972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7d44e3d7be51c713b9",
+    "url": "/werewolf-prototype/static/js/main.23568d2e.chunk.js"
+  },
+  {
+    "revision": "2bf6d2f0fb8c0d4992ae",
+    "url": "/werewolf-prototype/static/js/runtime-main.c7259c8b.js"
+  },
+  {
+    "revision": "6a7b8ba3441721a57e122b4c0b2fd05f",
+    "url": "/werewolf-prototype/static/media/MyMedieval.6a7b8ba3.ttf"
+  },
+  {
+    "revision": "a752d28690bc124802a778969e3b820b",
+    "url": "/werewolf-prototype/static/media/villager.a752d286.png"
+  },
+  {
+    "revision": "2c9f61d19212dc34a3156bb742019a19",
+    "url": "/werewolf-prototype/static/media/werewolf.2c9f61d1.png"
+  },
+  {
+    "revision": "573550079d00f8d44d2966fa3eba3713",
+    "url": "/werewolf-prototype/static/media/wood.57355007.jpg"
+  }
+]);
